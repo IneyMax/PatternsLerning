1fbff44faefeaa77a4ad369d37b5a2241775aa4b
Letov_Maxim <mr.algree@gmail.com>
2022-07-05 17:17:56 +0300

    Add base class for decorator

--- MyProject/Source/MyProject/Public/Factory/Factories/PotionShop.h
+++ MyProject/Source/MyProject/Public/Factory/Factories/PotionShop.h
@@ -15,7 +15,7 @@ class MYPROJECT_API UPotionShop : public UObject
 
 public:
 	//Create Potion and returns it. It's pure virtual, so it doesn't need an implementation in this class
-	virtual	APotion* ConcoctPotion(FString PotionSKU) PURE_VIRTUAL(APotionShop::ConcoctPotion, return nullptr;);
+	virtual	APotion* ConcoctPotion(FString PotionSKU) PURE_VIRTUAL(UPotionShop::ConcoctPotion, return nullptr;);
 
 	//Order, concoct and returns a Potion of a specific Category
 	APotion* OrderPotion(FString Category);
